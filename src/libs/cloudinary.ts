7d7b039f3775b5e0bee6d755e9087f153d777b94
natanaeldev20 <natanaelvasquez150103@gmail.com>
2025-08-10 20:24:48 -0500

    cloudinary

--- src/libs/cloudinary.ts
+++ src/libs/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dezz9ntar",
-  api_key: "616179364327118",
-  api_secret: "ciiH28Yw7r6avpKzt5cvcraoppk",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
