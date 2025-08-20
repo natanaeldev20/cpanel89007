d7b5619494917f892513c6c55c23feb7745cf162
natanaeldev20 <natanaelvasquez150103@gmail.com>
2025-08-19 21:28:52 -0500

    dynamic

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import SectionHeader from "@/components/ui/SectionHeader";
 import Section from "@/components/ui/Section";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
